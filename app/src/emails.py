5d6d535f3741b3ab89b6d0c563c937a019228d74
Horacio Ibrahim <horacioibrahim@gmail.com>
2015-06-19 23:20:33 -0300

    We done implemented support to SES AMAZON in the emails module

--- app/src/emails.py
+++ app/src/emails.py
@@ -5,6 +5,7 @@ Snnipet to send all emails in this app.
 created by horacioibrahim at gmail dot com
 created at 15/Jun/2015
 """
+from boto.ses.connection import SESConnection
 from flask_mail import Message
 from flask import render_template
 from flask.ext.babel import lazy_gettext as _
@@ -13,8 +14,13 @@ from src.decorators import async
 
 @async
 def async_send_mail(app, msg):
+    ses = SESConnection(
+            aws_access_key_id=app.config.get(AWS_ACCESS_KEY_ID),
+            aws_secret_access_key=app.config.get(AWS_SECRET_ACCESS_KEY)
+        )	
 	with app.app_context():
-		mail.send(msg)
+		#mail.send(msg)
+		ses.send_email(msg.sender, msg.subject, msg.body, msg.recipients, html_body=msg.html)
 
 def default_send_mail(subject, sender, recipients, body_text, body_html=None):
 	""" To send message by flask-mail lib using threading """
