d70c0fe11e842958ea26426fa4d8a996e6f70ad7
Horacio Ibrahim <horacioibrahim@gmail.com>
2015-06-26 09:48:13 -0300

    Changed template tags: bad use of the <

--- app/siscomando/__init__.py
+++ app/siscomando/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 import redis
-from flask import Flask
+from flask import Flask, Blueprint 
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.cors import CORS
 from flask.ext.login import LoginManager
@@ -15,18 +15,23 @@ red = redis.StrictRedis()
 class CustomFlask(Flask):
     jinja_options = Flask.jinja_options.copy()
     jinja_options.update(dict(
-        block_start_string='<%',
-        block_end_string='%>',
+        block_start_string='{%',
+        block_end_string='%}',
         variable_start_string='%%',
         variable_end_string='%%',
-        comment_start_string='<#',
-        comment_end_string='#>'
+        comment_start_string='{#',
+        comment_end_string='#}'
     ))
 
 
-#app = Flask(__name__)
 app = CustomFlask(__name__)
 app.config.from_envvar('SISCOMANDO_SETTINGS')
+
+# Settings webservice flask_restful entry point.
+webservice_bp = Blueprint('webservice', __name__)
+webservice = restful.Api(webservice_bp, prefix="/api/v2")
+
+# CORS to support external request (outside from hosted domain).
 cors = CORS(app, resources=r'/api/v1/*', origins='*', 
 	allow_headers=['Content-Type', 'Origin', 'Accept,', 'X-Requested-With', 
 				'X-CSRF-Token','Access-Control-Allow-Origin'])
@@ -34,9 +39,8 @@ cors = CORS(app, resources=r'/api/v1/*', origins='*',
 mail = Mail(app)
 # i18n and l10n
 babel = Babel(app)
-# Initiate API instance from restful
-api = restful.Api(app, prefix="/api/v2")
-# Load together Flask and Flask-login
+
+# Load together Flask and Flask-login.
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
@@ -46,9 +50,6 @@ database = MongoEngine(app)
 # Config cans to be used: app.config.from_object(__name__)
 # from_object() will look at the given object (if it’s a string it will import it) 
 # and then look for all uppercase variables defined there.
-# or
-# app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
 # Load views
 from siscomando import views
 
